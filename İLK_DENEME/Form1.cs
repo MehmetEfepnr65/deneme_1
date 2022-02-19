26062e1b30ea71d3f68c77271c5f2f73d622e01c
MehmetEfepnr65 <99246225+MehmetEfepnr65@users.noreply.github.com>
2022-02-19 16:02:45 +0300

    Add files via upload

---
+++ İLK_DENEME/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace İLK_DENEME
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int sayac = 0;
+            double ortalama;
+            int toplam = 0;
+            while(sayac < listBox1.Items.Count)
+            {
+                toplam=toplam+ Convert.ToInt32 (listBox1.Items[sayac]);
+                sayac++;
+            }
+            ortalama = toplam / sayac;
+            label1.Text = "ortalama:"+ortalama;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int sayac = 0;
+            double ortalama;
+            int toplam = 0;
+            while (sayac < listBox1.Items.Count)
+            {
+                if (Convert.ToInt32(listBox1.Items[sayac]) % 2 == 1) 
+                {
+                    break;
+                }
+                toplam = toplam + Convert.ToInt32(listBox1.Items[sayac]);
+                sayac++;
+            }
+            ortalama = toplam / sayac;
+            label1.Text = "ortalama:" + ortalama;
+        }
+    }
+}
